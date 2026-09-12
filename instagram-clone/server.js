08c777f6dc5130b78b0ff33b2953a0ca54ccaf3c
Mohan k <mohanarul2003@gmail.com>
2026-09-12 14:44:31 +0530

    MongoDB implement

--- instagram-clone/server.js
+++ instagram-clone/server.js
@@ -8,8 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb://localhost:27017/instagram";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/instagram";
 
 // Middleware
 app.use(cors());
